493cc174c296e78a9a8ac49656cce939beef0c42
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-04-04 16:56:12 -0300

    c

---
+++ custom_auth/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.functions import Now
+
+class CustomUser(AbstractUser):
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(data__lt=Now()),
+                name='data_nascimento_nao_pode_ser_em_datas_passadas'
+            )
+        ]
+
+    phone = models.CharField(max_length=50, unique=True)
+    address = models.TextField()
+    birth_date = models.DateField(blank=True, null=True)
