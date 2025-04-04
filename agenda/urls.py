8087d5a213d22f8ef52337b2b293120d5fea4237
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-04-04 16:24:48 -0300

    fixing url

--- agenda/urls.py
+++ agenda/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/servicos', views.criar_listar_servicos, name='listar_servicos'),
+    path('api/servicos/', views.criar_listar_servicos, name='listar_servicos'),
     path('api/servicos/', views.criar_servico, name='criar_servico'),
     path('api/servicos/<int:pk>', views.criar_consulta, name='criar_consulta'),
 
-    path('api/agendamentos', views.criar_listar_servicos, name='listar_servicos'),
+    path('api/agendamentos/', views.criar_listar_servicos, name='listar_servicos'),
     path('api/agendamentos/', views.criar_servico, name='criar_servico'),
     path('api/agendamentos/<int:pk>', views.criar_consulta, name='criar_consulta'),
 ]
--- servicos/urls.py
+++ servicos/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clinica.urls')),
-    path('', include('agenda.ursl'))
+    path('', include('agenda.urls'))
 ]
