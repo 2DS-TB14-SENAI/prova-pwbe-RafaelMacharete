caea6e6c8be01448fb2303a5d8d5ffd4f184ab1f
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-04-04 16:26:08 -0300

    fixing views name

--- agenda/urls.py
+++ agenda/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('api/servicos/', views.criar_listar_servicos, name='listar_servicos'),
-    path('api/servicos/', views.criar_servico, name='criar_servico'),
-    path('api/servicos/<int:pk>', views.criar_consulta, name='criar_consulta'),
+    path('api/servicos/', views.criar_listar_servicos, name='criar_servico'),
+    path('api/servicos/<int:pk>', views.criar_listar_servicos, name='criar_consulta'),
 
-    path('api/agendamentos/', views.criar_listar_servicos, name='listar_servicos'),
-    path('api/agendamentos/', views.criar_servico, name='criar_servico'),
-    path('api/agendamentos/<int:pk>', views.criar_consulta, name='criar_consulta'),
+    path('api/agendamentos/', views.criar_listar_agendamentos, name='listar_servicos'),
+    path('api/agendamentos/', views.criar_listar_agendamentos, name='criar_servico'),
+    path('api/agendamentos/<int:pk>', views.criar_listar_agendamentos, name='criar_consulta'),
 ]
